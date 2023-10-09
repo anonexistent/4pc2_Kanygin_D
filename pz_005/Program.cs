355ac6d505eb7781f1158513272a08b6cb68e78f
anonexistent <wwwzlx86@gmail.com>
2023-10-10 03:27:59 +0500

    4

--- pz_005/Program.cs
+++ pz_005/Program.cs
@@ -1,11 +1,12 @@
-﻿namespace pz_005
+﻿using System;
+
+namespace pz_005
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            printf("завянешь сука как цветы");
-            сщге ЮЮ жвсе мои секреты так и просятся наружуж;
+
         }
     }
 }
