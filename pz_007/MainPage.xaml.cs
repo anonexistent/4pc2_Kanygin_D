f1b2e7a9c308fcde75906feed168dcf2fab6fdf7
anonexistent <wwwzlx86@gmail.com>
2023-10-16 22:35:11 +0500

    пз 8

--- pz_007/MainPage.xaml.cs
+++ pz_007/MainPage.xaml.cs
@@ -1,29 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.WindowsRuntime;
+﻿using System.Runtime.InteropServices;
 using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
 
 namespace pz_007
 {
-
-
-    /// <summary>
-    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public struct MemoryStatusEx
@@ -59,18 +39,15 @@ namespace pz_007
 
             if(GlobalMemoryStatusEx(ref ms))
             {
-                while(true) 
-                {
-                    await Task.Delay(10);
-                    tbPh.Text = ms.ulTotalPhys + " byte";
-                    tbPhA.Text = ms.ulAvailPhys + " byte";
-                    tbPod.Text = ms.ulTotalPageFile + " byte";
-                    tbPodA.Text = ms.ulAvailPageFile + " byte"; ;
-                    tbVirt.Text = ms.ulTotalVirtual + " byte";
-                    tbVirtA.Text = ms.ulAvailVirtual + " byte";
-                    tbProc.Text = ms.ulAvailExtendedVirtual + " byte";
-                    tbLoad.Text = ms.uiMemoryLoad + " %";
-                }
+                await Task.Delay(10);
+                tbPh.Text = ms.ulTotalPhys + " byte";
+                tbPhA.Text = ms.ulAvailPhys + " byte";
+                tbPod.Text = ms.ulTotalPageFile + " byte";
+                tbPodA.Text = ms.ulAvailPageFile + " byte"; ;
+                tbVirt.Text = ms.ulTotalVirtual + " byte";
+                tbVirtA.Text = ms.ulAvailVirtual + " byte";
+                tbProc.Text = ms.ulAvailExtendedVirtual + " byte";
+                tbLoad.Text = ms.uiMemoryLoad + " %";
 
             }
 
---
+++ pz_008/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace pz_008
+{
+    class Program
+    {            
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr HeapCreate(UInt32 flOptions, UIntPtr dwInitialSize, UIntPtr dwMaximumSize);
+        public static void Main(string[] args)
+        {
+            ulong i = 0;
+
+            Thread t1 = new Thread(() =>
+            {
+                while (true)
+                {
+                    i++;
+                    Console.WriteLine("thread 1 work");
+                    Thread.Sleep(150);
+                    HeapCreate(0, (UIntPtr)64, (UIntPtr)128);
+                }
+            });
+            Thread t2 = new Thread(() =>
+            {
+                while (true)
+                {
+                    i++;
+                    Console.WriteLine("thread 2 work");
+                    Thread.Sleep(200);
+                    HeapCreate(0, (UIntPtr)64, (UIntPtr)128);
+                }
+            });
+            t1.Start();
+            t2.Start();
+        }
+    }
+}
